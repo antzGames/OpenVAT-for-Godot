16867a90025fce96d5f086bf67748937592d60e1
Antz <anthony.iantria@gmail.com>
2026-04-25 01:06:43 -0400

    Cleanup.

--- demo/scenes/src/open_vat_instanced_track.gd
+++ demo/scenes/src/open_vat_instanced_track.gd
@@ -5,8 +5,9 @@ extends OpenVATInstancedDemo
 
 func _ready() -> void:
 	super._ready()
+	mesh_floor.mesh.size = Vector2(200,200) 
 	for track in vat_multi_mesh_instance_3d.animation_tracks:
 		track_select.add_item(track.name)
 
 func _on_track_select_item_selected(index: int) -> void:
-	vat_multi_mesh_instance_3d.update_all_instances(0,index,1)
+	vat_multi_mesh_instance_3d.update_all_instances(0, index, 1)
